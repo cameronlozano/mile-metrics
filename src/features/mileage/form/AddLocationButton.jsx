3f6a4976b17c21ed8ffd4bde24eee7e8a8a527c3
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-08 18:12:40 -0500

    fix: 🐛 destructure correct ref variable from context

--- src/features/mileage/form/AddLocationButton.jsx
+++ src/features/mileage/form/AddLocationButton.jsx
@@ -2,12 +2,12 @@ import { Button } from 'react-aria-components';
 import { useFormContext } from './FormContext';
 
 const AddLocationButton = () => {
-  const { append, useLocationRef } = useFormContext();
+  const { append, addLocationRef } = useFormContext();
 
   return (
     <Button
       type='button'
-      ref={useLocationRef}
+      ref={addLocationRef}
       className='mx-auto w-fit cursor-pointer rounded-xs p-0.5 text-sm font-semibold text-gray-400 transition-all duration-100 outline-none hover:scale-105 hover:text-gray-500 focus-visible:ring-3 focus-visible:ring-emerald-500 active:scale-95 active:text-slate-400'
       onClick={() => {
         append({
