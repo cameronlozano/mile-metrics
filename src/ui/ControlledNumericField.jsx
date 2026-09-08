07a8fd3a641633dd64785b7af9e45361d699ed1c
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-07 19:58:33 -0500

    refactor: 💡 add tracking tighter

--- src/ui/ControlledNumericField.jsx
+++ src/ui/ControlledNumericField.jsx
@@ -41,7 +41,7 @@ const ControlledNumericField = ({
               />
 
               {fieldState.error && (
-                <p className='absolute top-10 left-0 text-[0.625rem] text-nowrap text-red-500'>
+                <p className='absolute top-10 left-2.5 text-[0.625rem] tracking-tighter text-nowrap text-red-500'>
                   {fieldState.error.message}
                 </p>
               )}
