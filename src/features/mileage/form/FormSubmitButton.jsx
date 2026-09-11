343cff2f6d433dc88fed1488d5f07ce4bb1c930a
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-11 17:38:55 -0500

    refactor: change rendered txt

--- src/features/mileage/form/FormSubmitButton.jsx
+++ src/features/mileage/form/FormSubmitButton.jsx
@@ -10,7 +10,7 @@ const FormSubmitButton = () => {
       disabled={isSubmitting}
       className='text-md mx-auto w-100 rounded-lg border border-gray-300 bg-emerald-600 p-2.5 text-center font-bold tracking-wider text-white ring-offset-2 transition-all duration-150 hover:cursor-pointer hover:bg-emerald-700 focus:ring-3 focus:ring-emerald-600 focus:outline-none active:scale-95 active:bg-emerald-500'
     >
-      {isSubmitting ? <span>...Submitting</span> : 'Submit'}
+      {isSubmitting ? <span>...saving record</span> : 'Create record'}
     </Button>
   );
 };
