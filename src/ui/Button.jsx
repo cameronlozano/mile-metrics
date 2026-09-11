14847d4c379e607c9fed20724dee92ad13e1bfd8
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-11 17:28:28 -0500

    refactor: begin to allow styling by variant

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,6 @@
 import { twMerge } from 'tailwind-merge';
 import { Button as BaseUIButton } from '@base-ui/react/button';
+import cn from '../utils/cn';
 
 export default function Button({
   type = 'button',
@@ -7,14 +8,25 @@ export default function Button({
   className = '',
   children = '',
   onClick = undefined,
+  variant,
 }) {
+  let variantStyling;
+  const stylingLookup = {
+    primary: cn(
+      'border border-gray-400 bg-emerald-600 px-3 py-3.5 font-semibold tracking-wider text-white',
+    ),
+  };
+
+  if (variant != null) variantStyling = stylingLookup[variant];
+
   return (
     <BaseUIButton
       type={type}
       disabled={disabled}
       className={twMerge(
-        'rounded-xl border-2 border-slate-500 bg-slate-50 p-3 text-center text-sm font-semibold tracking-wider text-slate-500 transition-all duration-150 hover:cursor-pointer hover:bg-slate-300 hover:text-slate-600 focus:outline-none focus-visible:border-emerald-500 focus-visible:shadow focus-visible:ring-3 focus-visible:ring-emerald-500 active:scale-95 active:border-slate-400/75 active:bg-slate-100 active:text-slate-600/75',
+        'flex cursor-pointer rounded-full text-center text-sm transition-all duration-100',
         className,
+        variant && variantStyling,
       )}
       onClick={onClick}
     >
