2d12a76d73d935d84382d5069f85fde1ae100adb
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-11 17:35:13 -0500

    refactor: adjust positioning of navlinks

--- src/features/mileage/MileageNavbar.jsx
+++ src/features/mileage/MileageNavbar.jsx
@@ -5,20 +5,20 @@ import cn from '../../utils/cn';
 export default function Navbar() {
   const navLinkStyling = (isActive) =>
     cn(
-      `relative flex items-center rounded-xs pb-1 font-semibold tracking-tight text-gray-600 outline-none after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:origin-center after:scale-x-0 after:bg-emerald-500 after:transition-transform after:duration-150 focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-3 focus-visible:ring-offset-gray-200`,
+      `absolute relative flex items-center rounded-xs font-bold tracking-tight text-gray-600 outline-none after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:origin-center after:scale-x-0 after:bg-emerald-500 after:transition-transform after:duration-150 focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-3 focus-visible:ring-offset-gray-200`,
 
       isActive && 'after:scale-x-100',
     );
 
   return (
-    <div className='mb-6 flex h-13 items-center justify-between gap-2 border-b border-b-gray-300 bg-gray-200 px-3 text-xs shadow-xs'>
+    <div className='z-50 flex h-13 items-center justify-between gap-2 px-3 text-xs'>
       <div className='flex place-items-center'>
         <NavLink className={({ isActive }) => navLinkStyling(isActive)} to='/'>
           <img src={logo} alt='MileMetrics' className='h-8' />
         </NavLink>
       </div>
 
-      <div className='flex h-8 gap-3 pr-2'>
+      <div className='flex h-8 gap-7 pr-20'>
         <NavLink
           className={({ isActive }) => cn(navLinkStyling(isActive))}
           to={'/add-miles'}
