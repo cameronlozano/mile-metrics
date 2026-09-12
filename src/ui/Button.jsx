3ff9fc75abba28fc57b2dabd564f039a15af6f99
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-12 11:12:01 -0500

    add hover & active states to primary variation

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -13,7 +13,7 @@ export default function Button({
   let variantStyling;
   const stylingLookup = {
     primary: cn(
-      'border border-gray-400 bg-emerald-600 px-3 py-3.5 font-semibold tracking-wider text-white',
+      'border border-gray-400 bg-emerald-600/90 px-3 py-3.5 font-semibold tracking-wider text-white transition-all duration-100 outline-none hover:bg-emerald-600 focus-visible:ring-3 focus-visible:ring-emerald-600 focus-visible:ring-offset-3 active:scale-95 active:bg-emerald-600/70',
     ),
   };
 
