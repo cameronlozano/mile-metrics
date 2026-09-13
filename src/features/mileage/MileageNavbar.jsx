d4c6dd722144f5ef3e6db073f5665c505ef2a644
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-13 14:23:58 -0500

    refactor: improve visual styling of focus states

--- src/features/mileage/MileageNavbar.jsx
+++ src/features/mileage/MileageNavbar.jsx
@@ -5,33 +5,30 @@ import cn from '../../utils/cn';
 export default function Navbar() {
   const navLinkStyling = (isActive) =>
     cn(
-      `text-color-header-tertiary absolute relative flex items-center rounded-xs font-medium tracking-tight outline-none after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:origin-center after:translate-y-1 after:scale-x-0 after:bg-emerald-500 after:transition-transform after:duration-150 focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-3 focus-visible:ring-offset-gray-200`,
+      `text-color-header-tertiary absolute relative flex rounded-sm font-medium tracking-tight outline-none after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:origin-center after:translate-y-1 after:scale-x-0 after:bg-emerald-500 after:transition-transform after:duration-150 focus-visible:ring-3 focus-visible:ring-emerald-500 focus-visible:ring-offset-slate-100`,
 
       isActive && 'after:scale-x-100',
     );
 
   return (
-    // LOGO
-    <div className='z-50 flex h-13 items-center justify-between gap-2 px-3 text-xs'>
-      <div className='flex place-items-center'>
-        <NavLink className={({ isActive }) => navLinkStyling(isActive)} to='/'>
-          <img src={logo} alt='MileMetrics' className='h-8' />
-        </NavLink>
-      </div>
+    <div className='z-50 flex h-13 items-center justify-between px-3 text-xs'>
+      {/* LOGO */}
+      <NavLink className={({ isActive }) => navLinkStyling(isActive)} to='/'>
+        <img src={logo} alt='MileMetrics' className='h-6.5' />
+      </NavLink>
 
-      <div className='ml-auto flex gap-9.25 pr-9'>
-        {/* LOGIN */}
-        <div>
-          <NavLink
-            className={({ isActive }) => cn(navLinkStyling(isActive))}
-            to={'/login'}
-          >
-            Login
-          </NavLink>
-        </div>
+      {/* LOGIN */}
+      <div className='ml-auto flex gap-8.5 pr-9'>
+        <NavLink
+          className={({ isActive }) => cn(navLinkStyling(isActive), 'p-0.5')}
+          to={'/login'}
+        >
+          Login
+        </NavLink>
+
         {/* ADD MILES */}
         <NavLink
-          className={({ isActive }) => cn(navLinkStyling(isActive))}
+          className={({ isActive }) => cn(navLinkStyling(isActive), 'p-0.5')}
           to={'/add-miles'}
         >
           Add Miles
@@ -39,7 +36,7 @@ export default function Navbar() {
 
         {/* VIEW MILES */}
         <NavLink
-          className={({ isActive }) => cn(navLinkStyling(isActive))}
+          className={({ isActive }) => cn(navLinkStyling(isActive), 'p-0.5')}
           to={'view-miles'}
         >
           View miles
