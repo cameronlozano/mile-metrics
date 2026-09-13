285df4040197518792daad9b32775c5308fe0441
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-13 12:23:16 -0500

    refactor: add login navLink and adjust positioning

--- src/features/mileage/MileageNavbar.jsx
+++ src/features/mileage/MileageNavbar.jsx
@@ -5,12 +5,13 @@ import cn from '../../utils/cn';
 export default function Navbar() {
   const navLinkStyling = (isActive) =>
     cn(
-      `absolute relative flex items-center rounded-xs font-bold tracking-tight text-gray-600 outline-none after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:origin-center after:scale-x-0 after:bg-emerald-500 after:transition-transform after:duration-150 focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-3 focus-visible:ring-offset-gray-200`,
+      `text-color-header-tertiary absolute relative flex items-center rounded-xs font-medium tracking-tight outline-none after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:origin-center after:translate-y-1 after:scale-x-0 after:bg-emerald-500 after:transition-transform after:duration-150 focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-3 focus-visible:ring-offset-gray-200`,
 
       isActive && 'after:scale-x-100',
     );
 
   return (
+    // LOGO
     <div className='z-50 flex h-13 items-center justify-between gap-2 px-3 text-xs'>
       <div className='flex place-items-center'>
         <NavLink className={({ isActive }) => navLinkStyling(isActive)} to='/'>
@@ -18,7 +19,17 @@ export default function Navbar() {
         </NavLink>
       </div>
 
-      <div className='flex h-8 gap-7 pr-20'>
+      <div className='ml-auto flex gap-9.25 pr-9'>
+        {/* LOGIN */}
+        <div>
+          <NavLink
+            className={({ isActive }) => cn(navLinkStyling(isActive))}
+            to={'/login'}
+          >
+            Login
+          </NavLink>
+        </div>
+        {/* ADD MILES */}
         <NavLink
           className={({ isActive }) => cn(navLinkStyling(isActive))}
           to={'/add-miles'}
@@ -26,6 +37,7 @@ export default function Navbar() {
           Add Miles
         </NavLink>
 
+        {/* VIEW MILES */}
         <NavLink
           className={({ isActive }) => cn(navLinkStyling(isActive))}
           to={'view-miles'}
