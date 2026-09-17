040a83f133f9bc0a042f9a0568ace0a78006e731
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-17 17:07:49 -0500

    feat: add bg-gradient for hero section

--- src/features/mileage/homepage/HeroSection.jsx
+++ src/features/mileage/homepage/HeroSection.jsx
@@ -5,15 +5,15 @@ import HeroImg from './HeroImg';
 
 const HeroSection = () => (
   <div className='grid grid-cols-1'>
-    <div className='xs:col-start-1 xs:row-start-1 pb-2.5'>
+    <div className='xs:col-start-1 xs:row-start-1 xs:pb-0 pb-2.5'>
       <HeroImg />
     </div>
 
-    <div className='xs:pb-5'>
+    <div className='xs:pb-5 xs:pt-2.5'>
       <HeroEyebrow />
     </div>
 
-    <div className='xs:col-start-1 xs:row-start-1 xs:pb-0.5 xs:pt-1.5 pb-4 pl-1.5'>
+    <div className='xs:col-start-1 xs:bg-linear-to-r xs:from-slate-100/50 xs:row-start-1 xs:pb-0.5 xs:pt-1.5 to-transparent pb-4 pl-1.5'>
       <Header />
     </div>
 
